8a88b1b1d39dc239feb5bec93e7ebf33b1f13447
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-13 18:29:54 +0100

    Feat: Create several segments in main UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 export default function Home() {
   return (
     <main className="">
+      <header></header>
+      <section></section>
+      <section></section>
+      <section></section>
+      <footer></footer>
     </main>
   )
 }
