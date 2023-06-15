eff31856b12857d9987051773bef72b83e67f180
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-15 18:48:03 +0100

    Fix: Remove unused style

--- app/components/responsive-nav.tsx
+++ app/components/responsive-nav.tsx
@@ -29,11 +29,11 @@ function ResponsiveNav() {
         <Transition
           as={Fragment}
           enter='transition ease-out duration-100 z-[-1]'
-          enterFrom='transform opacity-0 translate-x-full'
-          enterTo='transform opacity-100 translate-x-0'
+          enterFrom='transform translate-x-full'
+          enterTo='transform translate-x-0'
           leave='transition ease-in duration-75 z-[-1]'
-          leaveFrom='transform opacity-100 translate-x-0'
-          leaveTo='transform opacity-0 translate-x-full'
+          leaveFrom='transform translate-x-0'
+          leaveTo='transform translate-x-full'
         >
           <Menu.Items className='flex bg-zinc-200 w-full flex-col absolute z-[-1] top-[99%] left-0  justify-evenly items-start gap-7 px-[1%] py-5'>
             {links.map(({ address, href }) => (
