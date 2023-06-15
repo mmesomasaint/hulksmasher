23ffc401dc2bd65f8b0c42560a12c25aa6346605
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-15 18:19:31 +0100

    Fix: Format with prettier

--- app/page.tsx
+++ app/page.tsx
@@ -7,61 +7,54 @@ import ResponsiveNav from './components/responsive-nav'
 export default function Home() {
   return (
     <main className='min-h-screen'>
-    <header className='static sm:fixed sm:top-0 sm:inset-x-0 sm:z-50 w-full bg-zinc-200'>
-      <div className='relative max-w-[90rem] w-full'>
-        <div className='w-full lg:w-[90%] xl:w-[80%] mx-auto py-4 flex flex-wrap justify-between items-center gap-6'>
-        <div className='px-[1%] flex-shrink-0 w-full sm:w-fit'>
-          <Image
-            src='/1-shoe.png'
-            width={50}
-            height={30}
-            alt='logo'
-          />
-        </div>
-        <div className='col-span-3 flex justify-between items-center gap-7'>
-          <div
-            className='w-fit flex-row bg-transparent hidden lg:flex justify-evenly items-center gap-7 p-0'
-          >
-            <span className='font-normal text-lg leading-none text-primary'>
-              <Link href='/'>Home</Link>
-            </span>
-            <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
-              <Link href='/discover'>Discover</Link>
-            </span>
-            <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
-              <Link href='/patners'>Patners</Link>
-            </span>
-            <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
-              <Link href='/about'>About</Link>
-            </span>
-            <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
-              <Link href='/feedback'>Feedbacks</Link>
-            </span>
-            <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
-              <Link href='/contact'>Contact</Link>
-            </span>
+      <header className='static sm:fixed sm:top-0 sm:inset-x-0 sm:z-50 w-full bg-zinc-200'>
+        <div className='relative max-w-[90rem] w-full'>
+          <div className='w-full lg:w-[90%] xl:w-[80%] mx-auto py-4 flex flex-wrap justify-between items-center gap-6'>
+            <div className='px-[1%] flex-shrink-0 w-full sm:w-fit'>
+              <Image src='/1-shoe.png' width={50} height={30} alt='logo' />
+            </div>
+            <div className='col-span-3 flex justify-between items-center gap-7'>
+              <div className='w-fit flex-row bg-transparent hidden lg:flex justify-evenly items-center gap-7 p-0'>
+                <span className='font-normal text-lg leading-none text-primary'>
+                  <Link href='/'>Home</Link>
+                </span>
+                <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
+                  <Link href='/discover'>Discover</Link>
+                </span>
+                <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
+                  <Link href='/patners'>Patners</Link>
+                </span>
+                <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
+                  <Link href='/about'>About</Link>
+                </span>
+                <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
+                  <Link href='/feedback'>Feedbacks</Link>
+                </span>
+                <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
+                  <Link href='/contact'>Contact</Link>
+                </span>
+              </div>
+            </div>
+            <div className='sticky top-0 z-50 flex justify-between sm:justify-end items-center w-full sm:w-fit gap-3 bg-zinc-200'>
+              <div className='block lg:hidden'>
+                <ResponsiveNav />
+              </div>
+              <button className='flex-shrink-0 flex justify-center items-center gap-2 w-fit sm:w-10 h-10 px-2 sm:px-0 mr-[1%] sm:mr-0 bg-black rounded-xl'>
+                <BsTelephone className='text-white text-lg sm:text-xl' />
+                <span className='sm:hidden text-white text-base'>Call Us</span>
+              </button>
+              <div className='hidden sm:flex flex-col justify-center items-start gap-2 mr-[2%] lg:mr-0'>
+                <span className='font-light text-base leading-none text-black'>
+                  Call Us
+                </span>
+                <span className='font-normal text-lg leading-none text-black'>
+                  08157832809
+                </span>
+              </div>
+            </div>
           </div>
         </div>
-        <div className='sticky top-0 z-50 flex justify-between sm:justify-end items-center w-full sm:w-fit gap-3 bg-zinc-200'>
-          <div className='block lg:hidden'>
-            <ResponsiveNav />
-          </div>
-          <button className='flex-shrink-0 flex justify-center items-center gap-2 w-fit sm:w-10 h-10 px-2 sm:px-0 mr-[1%] sm:mr-0 bg-black rounded-xl'>
-            <BsTelephone className='text-white text-lg sm:text-xl' />
-            <span className='sm:hidden text-white text-base'>Call Us</span>
-          </button>
-          <div className='hidden sm:flex flex-col justify-center items-start gap-2 mr-[2%] lg:mr-0'>
-            <span className='font-light text-base leading-none text-black'>
-              Call Us
-            </span>
-            <span className='font-normal text-lg leading-none text-black'>
-              08157832809
-            </span>
-          </div>
-        </div>
-      </div>
-      </div>
-    </header>
+      </header>
       <section className='max-w-[90rem] mx-auto'>
         <div className='relative overflow-hidden h-fit md:h-[40rem] w-full bg-zinc-900'>
           <div className='flex justify-center items-center h-full'>
@@ -105,12 +98,7 @@ export default function Home() {
           </div>
           <div className='absolute origin-top-right -rotate-45 top-0 z-30 w-full h-full border border-red-500'>
             <div className='relative w-[50%] ml-[50%] h-full'>
-              <Image
-                src='/shirt-one.png'
-                fill
-                sizes='55vw'
-                alt='one shirt'
-              />
+              <Image src='/shirt-one.png' fill sizes='55vw' alt='one shirt' />
             </div>
           </div>
         </div>
