62b1c9c9c5a51d0ea364ccb70273896aac7a0e57
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-15 14:35:53 +0100

    Fix: Format with prettier

--- app/components/responsive-nav.tsx
+++ app/components/responsive-nav.tsx
@@ -13,55 +13,64 @@ function ResponsiveNav() {
     <Menu>
       <div>
         <Menu.Button>
-        <GiHamburgerMenu className='text-3xl text-black' onClick={handleOpen} />
+          <GiHamburgerMenu
+            className='text-3xl text-black'
+            onClick={handleOpen}
+          />
         </Menu.Button>
         <Transition
-                    as={Fragment}
-                    enter='transition ease-out duration-100'
-                    enterFrom='transform opacity-0'
-                    enterTo='transform opacity-100'
-                    leave='transition ease-in duration-75'
-                    leaveFrom='transform opacity-100'
-                    leaveTo='transform opacity-0'>
-                      <Menu.Items className='flex bg-zinc-200 w-full flex-col absolute z-10 top-[99%] left-0  justify-evenly items-start gap-7 px-[1%] py-5'>
-                        <Menu.Item>
-                        {({ active }) => (
-        <span className='font-normal text-lg leading-none text-primary'>
-          <Link href='/'>Home</Link>
-        </span>
-                        )}
-                        </Menu.Item>
-                        <Menu.Item>
-                        {({ active }) => (
-        <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
-          <Link href='/discover'>Discover</Link>
-        </span>
-                        )}</Menu.Item>
-                        <Menu.Item>
-                        {({ active }) => (
-        <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
-          <Link href='/patners'>Patners</Link>
-        </span>
-                        )}</Menu.Item>
-                        <Menu.Item>
-                        {({ active }) => (
-        <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
-          <Link href='/about'>About</Link>
-        </span>
-                        )}</Menu.Item>
-                        <Menu.Item>
-                        {({ active }) => (
-        <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
-          <Link href='/feedback'>Feedbacks</Link>
-        </span>
-                        )}</Menu.Item>
-                        <Menu.Item>
-                        {({ active }) => (
-        <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
-          <Link href='/contact'>Contact</Link>
-        </span>
-                        )}</Menu.Item>
-                      </Menu.Items>
+          as={Fragment}
+          enter='transition ease-out duration-100'
+          enterFrom='transform opacity-0'
+          enterTo='transform opacity-100'
+          leave='transition ease-in duration-75'
+          leaveFrom='transform opacity-100'
+          leaveTo='transform opacity-0'
+        >
+          <Menu.Items className='flex bg-zinc-200 w-full flex-col absolute z-10 top-[99%] left-0  justify-evenly items-start gap-7 px-[1%] py-5'>
+            <Menu.Item>
+              {({ active }) => (
+                <span className='font-normal text-lg leading-none text-primary'>
+                  <Link href='/'>Home</Link>
+                </span>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
+                  <Link href='/discover'>Discover</Link>
+                </span>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
+                  <Link href='/patners'>Patners</Link>
+                </span>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
+                  <Link href='/about'>About</Link>
+                </span>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
+                  <Link href='/feedback'>Feedbacks</Link>
+                </span>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <span className='font-normal text-lg leading-none text-black hover:text-gray-600'>
+                  <Link href='/contact'>Contact</Link>
+                </span>
+              )}
+            </Menu.Item>
+          </Menu.Items>
         </Transition>
       </div>
     </Menu>
