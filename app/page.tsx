cc614e285d692bebe0b72add208ffa4e02943cf3
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-15 18:42:35 +0100

    Fix: Remove unused style

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main>
       <div className='min-h-screen'>
         <header className='static sm:fixed sm:top-0 sm:inset-x-0 sm:z-50 w-full bg-zinc-200'>
-          <div className='relative max-w-[90rem] w-full'>
+          <div className='max-w-[90rem] w-full'>
             <div className='w-full lg:w-[90%] xl:w-[80%] mx-auto py-4 flex flex-wrap justify-between items-center gap-6'>
               <div className='px-[1%] flex-shrink-0 w-full sm:w-fit'>
                 <Image src='/1-shoe.png' width={50} height={30} alt='logo' />
