3492176e174f7cfabe4d39ab90f7fda9ac4c6725
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-16 18:47:53 +0100

    Feat: Design the button component

---
+++ app/components/button.tsx
@@ -0,0 +1,14 @@
+type ButtonType = {
+  children: React.ReactNode
+  secondary: boolean | undefined
+}
+
+function Button({children, secondary}: ButtonType) {
+  return (
+    <button className={`${secondary ? 'bg-amber-500' : 'bg-red-500'} px-6 py-3 rounded-md text-base font-medium text-white`}>
+      {children}
+    </button>
+  )
+}
+
+export default Button
