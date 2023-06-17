a421f5a7f0355670d43432acbcf0ac3a439b295d
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-17 16:57:49 +0100

    Feat: make the padding x align with the parent

--- app/page.tsx
+++ app/page.tsx
@@ -104,7 +104,7 @@ export default function Home() {
                   <Image src='/workplace-artisan.jpg' width={250} height={200} alt='about industry' className='absolute top-0 left-0' />
                   <Image src='/classy-man-tailor-shop.jpg' width={450} height={400} alt='about industry employees' className='absolute top-[150px] xl:top-[100px] right-0' />
                 </div>
-                <div className='flex order-first md:order-none flex-col justify-start items-start gap-5 px-10'>
+                <div className='flex order-first md:order-none flex-col justify-start items-start gap-5 px-[2%] md:px-10'>
                   <h3 className='text-base font-medium text-black leading-none'>What We Are About</h3>
                   <h2 className='text-4xl font-bold text-red-500 leading-tight'>We Dress Our Clients For Any Occasion</h2>
                   <p className='text-black text-base font-medium leading-tight'>We take pride in our work and the satisfaction of our clients. For every type of cloth and material we turn them into the best experience.</p>
