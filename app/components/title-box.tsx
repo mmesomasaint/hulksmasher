a381348039f31c6b790503cd7ab8716fc9ade523
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-23 19:08:19 +0100

    Fix: Format with prettier

--- app/components/title-box.tsx
+++ app/components/title-box.tsx
@@ -1,17 +1,16 @@
 function TitleBox() {
   return (
     <div className='flex order-first md:order-none flex-col justify-start items-start gap-5 px-[2%] md:px-10'>
-                    <h3 className='text-sm sm:text-base font-medium text-black leading-none'>
-                      What We Are About
-                    </h3>
-                    <h2 className='text-3xl sm:text-4xl font-bold text-red-500 leading-tight'>
-                      We Dress Our Clients For Any Occasion
-                    </h2>
-                    <p className='text-black text-sm sm:text-base font-medium leading-tight'>
-                      We take pride in our work and the satisfaction of our
-                      clients. For every type of cloth and material we turn them
-                      into the best experience.
-                    </p>
-                    </div>
+      <h3 className='text-sm sm:text-base font-medium text-black leading-none'>
+        What We Are About
+      </h3>
+      <h2 className='text-3xl sm:text-4xl font-bold text-red-500 leading-tight'>
+        We Dress Our Clients For Any Occasion
+      </h2>
+      <p className='text-black text-sm sm:text-base font-medium leading-tight'>
+        We take pride in our work and the satisfaction of our clients. For every
+        type of cloth and material we turn them into the best experience.
+      </p>
+    </div>
   )
 }
