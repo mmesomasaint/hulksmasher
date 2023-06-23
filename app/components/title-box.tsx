90ba2ff3b49084fa336b94531cbb2de74c96351f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-23 19:12:14 +0100

    Feat: Make title box a component

--- app/components/title-box.tsx
+++ app/components/title-box.tsx
@@ -1,15 +1,20 @@
-function TitleBox() {
+type TitleBoxType = {
+  intro: string 
+  main: string
+  children: React.ReactNode
+}
+
+function TitleBox({intro, main, children}: TitleBoxType) {
   return (
     <div className='flex order-first md:order-none flex-col justify-start items-start gap-5 px-[2%] md:px-10'>
       <h3 className='text-sm sm:text-base font-medium text-black leading-none'>
-        What We Are About
+        {intro}
       </h3>
       <h2 className='text-3xl sm:text-4xl font-bold text-red-500 leading-tight'>
-        We Dress Our Clients For Any Occasion
+        {main}
       </h2>
       <p className='text-black text-sm sm:text-base font-medium leading-tight'>
-        We take pride in our work and the satisfaction of our clients. For every
-        type of cloth and material we turn them into the best experience.
+        {children}
       </p>
     </div>
   )
