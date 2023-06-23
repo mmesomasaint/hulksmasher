586b3144ff4acd6202cedd536aad7244ff005b69
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-23 19:21:27 +0100

    Fix: Format with prettier

--- app/components/title-box.tsx
+++ app/components/title-box.tsx
@@ -1,13 +1,19 @@
 type TitleBoxType = {
   intro: string
   main: string
-  pos: "LEFT" | "RIGHT" | "CENTER"
+  pos: 'LEFT' | 'RIGHT' | 'CENTER'
   children: React.ReactNode
 }
 
 function TitleBox({ intro, main, pos, children }: TitleBoxType) {
   return (
-    <div className={`flex flex-col justify-start ${pos === "LEFT" && 'items-start'} ${pos === "RIGHT" && 'items-end'} ${pos === "CENTER" && 'items-center'} gap-5`}>
+    <div
+      className={`flex flex-col justify-start ${
+        pos === 'LEFT' && 'items-start'
+      } ${pos === 'RIGHT' && 'items-end'} ${
+        pos === 'CENTER' && 'items-center'
+      } gap-5`}
+    >
       <h3 className='text-sm sm:text-base font-medium text-black leading-none'>
         {intro}
       </h3>
