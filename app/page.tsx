d7602312dd6c98a1a2d2526dc30e0cc443c0d12c
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-23 18:56:29 +0100

    Feat: Modify the srcset variable

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,14 @@ import ResponsiveNav from './components/responsive-nav'
 import Button from './components/button'
 import CraftCard from './components/craft-card'
 
+type SrcType = {
+  src: string 
+  occasion: string
+  title: string
+}
+
 export default function Home() {
-  const srcset = ['/african-elder.jpg', '/african-woman-looking-up.jpg','/african-woman-wearing-traditional.jpg', '/caucasian-african-dress.jpg', '/man-wearing-african-traditional.jpg']
+  const srcset: SrcType[] = [{src: '/african-elder.jpg', occasion: 'August Union', title: 'Ichie'}, {src: '/african-woman-looking-up.jpg', occasion: 'Photo Shoot', title: 'Nwa Ada'}, {src: '/african-woman-wearing-traditional.jpg', occasion: 'Umuada Union', title: 'Umuada'}, {src: '/caucasian-african-dress.jpg', occasion: 'Afro Wedding', title: 'Brides Maid'}, {src: '/man-wearing-african-traditional.jpg', occasion: 'Senate Visit', title: 'Member'}]
 
   return (
     <main>
@@ -191,12 +197,12 @@ export default function Home() {
                 experience.
               </p>
               <div className='grid grid-cols-5 place-items-stretch gap-5 mt-5'>
-                {['1', '2', '3', '4', '5'].map((key) => (
+                {srcset.map(({src, occasion, title}) => (
                   <CraftCard
-                    key={key}
-                    src='/designer-with-tab.jpg'
-                    title='Employee'
-                    occasion='Workplace'
+                    key={`${src} ${occasion} ${title}`}
+                    src={src}
+                    title={title}
+                    occasion={occasion}
                   />
                 ))}
               </div>
