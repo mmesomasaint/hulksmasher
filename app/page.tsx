e3bc38333a2b0d616953c07729fe9f25a777cc59
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-24 07:07:25 +0100

    Fix: Format with prettier

--- app/page.tsx
+++ app/page.tsx
@@ -215,16 +215,16 @@ export default function Home() {
               </TitleBox>
               <div className='grid grid-cols-4 gap-10 place-items-stretch'>
                 <div className='group flex flex-col justify-start items-center gap-5 bg-amber-500/10 hover:bg-transparent shadow-[0_0_20px_-6px_rgb(239_68_68_/_0.55)] hover:shadow-[inset_0_0_30px_-12px_rgb(245_158_11_/_0.55),_0_0_50px_-6px_rgb(239_68_68_/_0.55)] rounded-2xl p-5'>
-                <div className='w-20 h-20 flex shrink-0 justify-center items-center rounded-2xl border border-zinc-600' />
-                <span className='text-sm sm:text-base font-semibold text-black group-hover:text-red-500 text-center leading-none'>
-                          Request Service
-                        </span>
-                        <span className='text-black text-xs sm:text-sm font-normal leading-tight text-center'>
-                          Contact us with deisgn or description.
-                        </span>
-                        <div className='w-10 h-10 flex justify-center items-center rounded-full bg-transparent group-hover:bg-red-500 border border-red-500/40'>
-                        <HiArrowSmRight className='text-lg xs:text-xl text-red-500 group-hover:text-white' />
-                        </div>
+                  <div className='w-20 h-20 flex shrink-0 justify-center items-center rounded-2xl border border-zinc-600' />
+                  <span className='text-sm sm:text-base font-semibold text-black group-hover:text-red-500 text-center leading-none'>
+                    Request Service
+                  </span>
+                  <span className='text-black text-xs sm:text-sm font-normal leading-tight text-center'>
+                    Contact us with deisgn or description.
+                  </span>
+                  <div className='w-10 h-10 flex justify-center items-center rounded-full bg-transparent group-hover:bg-red-500 border border-red-500/40'>
+                    <HiArrowSmRight className='text-lg xs:text-xl text-red-500 group-hover:text-white' />
+                  </div>
                 </div>
               </div>
             </div>
