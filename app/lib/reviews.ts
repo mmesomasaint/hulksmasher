9eb470e08e7327d5be0ffa9950ed42c39088d68b
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-25 20:06:09 +0100

    Fix: Format with prettier

--- app/lib/reviews.ts
+++ app/lib/reviews.ts
@@ -1,10 +1,35 @@
 export type ReviewType = {
   name: string
-  role: string 
+  role: string
   star: number
   said: string
 }
 
-const Reviews: ReviewType[] = [{name: 'Armani Kay', role: 'Project Manager', star: 4, said: 'Working with [company] has been a great success. They treat and loop you in through every process of the design'}, {name: 'Armani Kay', role: 'Project Manager', star: 4, said: 'Working with [company] has been a great success. They treat and loop you in through every process of the design'}, {name: 'Armani Kay', role: 'Project Manager', star: 4, said: 'Working with [company] has been a great success. They treat and loop you in through every process of the design'}, {name: 'Armani Kay', role: 'Project Manager', star: 4, said: 'Working with [company] has been a great success. They treat and loop you in through every process of the design'}]
+const Reviews: ReviewType[] = [
+  {
+    name: 'Armani Kay',
+    role: 'Project Manager',
+    star: 4,
+    said: 'Working with [company] has been a great success. They treat and loop you in through every process of the design',
+  },
+  {
+    name: 'Armani Kay',
+    role: 'Project Manager',
+    star: 4,
+    said: 'Working with [company] has been a great success. They treat and loop you in through every process of the design',
+  },
+  {
+    name: 'Armani Kay',
+    role: 'Project Manager',
+    star: 4,
+    said: 'Working with [company] has been a great success. They treat and loop you in through every process of the design',
+  },
+  {
+    name: 'Armani Kay',
+    role: 'Project Manager',
+    star: 4,
+    said: 'Working with [company] has been a great success. They treat and loop you in through every process of the design',
+  },
+]
 
 export default Reviews
