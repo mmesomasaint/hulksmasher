2e6825fc343dec280cf8060e0683da3d086b3ed2
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-25 19:56:50 +0100

    Feat: Design the pair animation

--- app/components/review.tsx
+++ app/components/review.tsx
@@ -1,4 +1,4 @@
-import {useMemo} from 'react'
+import {useState, useMemo} from 'react'
 import { ReviewType } from '../lib/reviews'
 
 type ReviewNodeType = {
@@ -6,6 +6,9 @@ type ReviewNodeType = {
 }
 
 function Review({reviews}: ReviewNodeType) {
+  const [activePair, setActivePair] = useState<number>(0)
+  const MID_ID = reviews.length / 2 - 1
+
   const pairedReviews: ReviewType[][] = useMemo(() => {
     const list: ReviewType[][] = []
     for(let i = 0; i < reviews.length - 1; i+=2) {
@@ -17,4 +20,50 @@ function Review({reviews}: ReviewNodeType) {
     }
     return list
   }, [])
+
+  return (
+    <>
+      {
+        pairedReviews.map((pair, id, list) => {
+          
+          const [ENDPOINT, MAIN, STARTPOINT] = [
+            'right-[110%]',
+            'right-[50%]',
+            '-right-[10%]',
+          ]
+
+          const PREV: number =
+            activePair === 0 ? list.length - 1 : activePair - 1
+
+          const NEXT: number =
+            activePair === list.length - 1 ? 0 : activePair + 1
+
+          let style: string = `${STARTPOINT}`
+          let hide: boolean = false
+
+          // Handle the style to use on each avatar based on activePair
+          if (id === activePair) {
+            style = `${MAIN}`
+          } else if (id === NEXT) {
+            style = `${STARTPOINT}`
+          } else if (id === PREV) {
+            style = `${ENDPOINT}`
+          } else {
+            hide = true
+
+            // Balancing - Make sure the activePair is at the center.
+            if (activePair < MID_ID && id > activePair + MID_ID) {
+              style = `${ENDPOINT}`
+            } else if (activePair > MID_ID && id < activePair - MID_ID) {
+              style = `${STARTPOINT}`
+            }
+          }
+
+          return (
+            <></>
+          )
+        })
+      }
+    </>
+  )
 }
