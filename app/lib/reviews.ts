d85af06495f77960430c3db1e00343f728fd8968
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-25 20:55:47 +0100

    Fix: Add imgSrc to reviewType

--- app/lib/reviews.ts
+++ app/lib/reviews.ts
@@ -1,4 +1,5 @@
 export type ReviewType = {
+  imgSrc: string
   name: string
   role: string
   star: number
@@ -7,24 +8,28 @@ export type ReviewType = {
 
 const Reviews: ReviewType[] = [
   {
+    imgSrc: './asset-tailors.jpg',
     name: 'Armani Kay',
     role: 'Project Manager',
     star: 4,
     said: 'Working with [company] has been a great success. They treat and loop you in through every process of the design',
   },
   {
+    imgSrc: './asset-tailors.jpg',
     name: 'Armani Kay',
     role: 'Project Manager',
     star: 4,
     said: 'Working with [company] has been a great success. They treat and loop you in through every process of the design',
   },
   {
+    imgSrc: './asset-tailors.jpg',
     name: 'Armani Kay',
     role: 'Project Manager',
     star: 4,
     said: 'Working with [company] has been a great success. They treat and loop you in through every process of the design',
   },
   {
+    imgSrc: './asset-tailors.jpg',
     name: 'Armani Kay',
     role: 'Project Manager',
     star: 4,
