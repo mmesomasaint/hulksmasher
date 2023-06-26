87d0773b0aa8d2ec3e5d059569245fee8e85bed2
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-26 05:34:53 +0100

    Fix: Positioning

--- app/components/review-card.tsx
+++ app/components/review-card.tsx
@@ -12,9 +12,9 @@ function ReviewCard({
   review: { imgSrc, name, role, star, said },
 }: ReviewCardType) {
   return (
-    <div className='flex flex-col justify-start items-start gap-5 h-fit my-auto p-5 mx-10 bg-amber-500/10 shadow-[0_0_20px_-6px_rgb(239_68_68_/_0.55)] rounded-2xl'>
-      <div className='grid grid-cols-3 place-items-end items-start gap-10'>
-        <div className='col-span-2 grid grid-cols-2 gap-5 place-items-stretch'>
+    <div className='flex flex-col justify-start items-start gap-3 h-fit my-auto p-5 mx-10 bg-amber-500/10 shadow-[0_0_20px_-6px_rgb(239_68_68_/_0.55)] rounded-2xl'>
+      <div className='grid grid-cols-3 place-items-end items-start gap-5'>
+        <div className='col-span-2 grid grid-cols-2 gap-1 place-items-stretch'>
           <Image
             src={imgSrc}
             width={120}
