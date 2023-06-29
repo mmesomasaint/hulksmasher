4b10c746dc5490baa3622eef053d29b760ec7191
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-29 22:35:37 +0100

    Fix: Add shadow

--- app/components/craft-card.tsx
+++ app/components/craft-card.tsx
@@ -8,7 +8,7 @@ type CraftCardType = {
 
 function CraftCard({src, title, occasion}: CraftCardType) {
   return (
-    <div className='rounded-2xl border border-black/40 hover:border-red-500 flex flex-col justify-between items-start'>
+    <div className='rounded-2xl flex flex-col justify-between items-start bg-amber-500/10 shadow-[0_0_20px_-6px_rgb(239_68_68_/_0.55)]'>
       <Image src={src} width={200} height={200} alt='employee' className='flex-grow rounded-t-2xl'/>
       <div className='flex flex-col gap-2 justify-start items-start p-3'>
         <span className='text-sm font-light text-black leading-none'><span className='text-black font-semibold'>Occasion:</span>&nbsp;{occasion}</span>
