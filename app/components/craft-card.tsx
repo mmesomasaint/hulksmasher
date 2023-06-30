015d062c3aa135b7f4d168b5101891fefe1f1ba0
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-30 15:07:03 +0100

    Fix: Format with prettier

--- app/components/craft-card.tsx
+++ app/components/craft-card.tsx
@@ -3,16 +3,27 @@ import Image from 'next/image'
 type CraftCardType = {
   src: string
   title: string
-  occasion: string 
+  occasion: string
 }
 
-function CraftCard({src, title, occasion}: CraftCardType) {
+function CraftCard({ src, title, occasion }: CraftCardType) {
   return (
     <div className='rounded-2xl flex flex-col justify-between items-start bg-amber-500/10 shadow-[0_0_20px_-6px_rgb(239_68_68_/_0.55)]'>
-      <Image src={src} width={200} height={200} alt='employee' className='flex-grow rounded-t-2xl'/>
+      <Image
+        src={src}
+        width={200}
+        height={200}
+        alt='employee'
+        className='flex-grow rounded-t-2xl'
+      />
       <div className='flex flex-col gap-2 justify-start items-start p-3'>
-        <span className='text-sm font-light text-black leading-none'><span className='text-black font-semibold'>Occasion:</span>&nbsp;{occasion}</span>
-        <span className='text-sm font-light text-black leading-none'><span className='text-black font-semibold'>Title:</span>&nbsp;{title}</span>
+        <span className='text-sm font-light text-black leading-none'>
+          <span className='text-black font-semibold'>Occasion:</span>&nbsp;
+          {occasion}
+        </span>
+        <span className='text-sm font-light text-black leading-none'>
+          <span className='text-black font-semibold'>Title:</span>&nbsp;{title}
+        </span>
       </div>
     </div>
   )
