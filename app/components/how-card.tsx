195631713c6e0e26bd50297f480ba5893c7badca
Mmesoma Saint <mmesomasaint@gmail.com>
2023-06-30 07:49:50 +0100

    Fix: Format with prettier

--- app/components/how-card.tsx
+++ app/components/how-card.tsx
@@ -10,7 +10,9 @@ function HowCard({ title, icon, children }: HowCardType) {
   return (
     <div className='group flex flex-col justify-start items-center gap-5 bg-amber-500/10 hover:bg-transparent shadow-[0_0_20px_-6px_rgb(239_68_68_/_0.55)] hover:shadow-[inset_0_0_30px_-12px_rgb(245_158_11_/_0.55),_0_0_50px_-6px_rgb(239_68_68_/_0.55)] rounded-2xl p-5'>
       <div className='w-20 h-20 flex shrink-0 justify-center items-center rounded-2xl bg-transparent group-hover:bg-red-500 border border-red-500/40 group-hover:border-red-500'>
-        <span className='text-6xl text-red-500 group-hover:text-white font-extrabold'>{icon}</span>
+        <span className='text-6xl text-red-500 group-hover:text-white font-extrabold'>
+          {icon}
+        </span>
       </div>
       <span className='text-sm sm:text-base font-semibold text-black group-hover:text-red-500 text-center leading-none'>
         {title}
